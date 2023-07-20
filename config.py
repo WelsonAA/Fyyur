1b8a1033e047f669cc1c0df87cd5a526a080d4a5
George Welson <georgeezwelson@outlook.com>
2023-07-21 02:13:26 +0300

    Many-to-Many Relationship Database connected Requirements modified

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from app import app
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,4 +11,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:george@localhost:5432/fyyur"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
